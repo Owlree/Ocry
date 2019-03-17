0ff9eb3f415bb843f8f32b994040f1d4141b5a9d
Robert Badea <owlree128@gmail.com>
2019-03-17 14:30:39 +0000

    Clean middlewares file (2)

--- docper/docper/middlewares.py
+++ docper/docper/middlewares.py
@@ -1,20 +1,19 @@
-# -*- coding: utf-8 -*-
+"""All middlewares for docper are described and implemented in this module.
+"""
 
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 import scrapy
 
-from scrapy import signals
 
-""" Enhances the requests with a depth property to allow DEPTH_LIMIT setting to
+"""Enhances the requests with a depth property to allow DEPTH_LIMIT setting to
 work with spiders that inherit from CrawlSpider.
+
+This comes as a workaround to the fact that CrawlSpider does not propagate 
+depth poperly.
 """
 class StickyDepthSpiderMiddleware:
 
-    """ Takes the depth property from the response and adds it to all outgoing
+    """Takes the depth property from the response and adds it to all outgoing
     requests.
     """
     def process_spider_output(self, response, result, spider):
