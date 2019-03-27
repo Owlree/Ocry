207ab39ebd8401f0eb206b349fd2348713c23e97
Robert Badea <owlree128@gmail.com>
2019-03-27 12:16:43 +0000

    S3 upload improvements

--- docper/pipelines.py
+++ docper/pipelines.py
@@ -1,8 +1,9 @@
+import os
 import json
 import scrapy
 import boto3
 import scrapy.pipelines.files
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 
 
 # TODO(Owlree): Move this to settings maybe
@@ -15,18 +16,19 @@ _DOMAIN_NAMES = {
 """We defined this class because we want to save some metadata [...]
 """
 class DocperFilesPipeline(scrapy.pipelines.files.FilesPipeline):
-    
+
     """We override this because we want so save metadatata for all downloaded
     files.
     """
     def item_completed(self, results, item, info):
         files = [x for ok, x in results if ok]
         if not files:
-            raise scrapy.exceptions.DropItem("Item contains no files")
-        item['files'] = files
+            raise scrapy.exceptions.DropItem('Item contains no files')
         del item['file_urls']
+        item['files'] = files
+        item['downloaded'] = True
         return item
-        
+
     """We override this because the 'full' directory was not very expressive,
     we want to replace it with something that is.
     """
@@ -39,12 +41,24 @@ class DocperFilesPipeline(scrapy.pipelines.files.FilesPipeline):
 
 class DynamoDBMetadata:
     def process_item(self, item, spider):
-        dynamodb = boto3.resource('dynamodb',)
-        table = dynamodb.Table('Docs')
-        
+        if not item.get('downloaded', False):
+            return item
+        settings = scrapy.utils.project.get_project_settings()
+
+        dynamodb = boto3.resource(
+            'dynamodb',
+            aws_access_key_id=settings.get('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=settings.get('AWS_SECRET_ACCESS_KEY'),
+            region_name=settings.get('AWS_REGION_NAME'))
+        table = dynamodb.Table(settings.get('AWS_DYNAMODB_METADATA_TABLE'))
+        s3path: str = settings.get('FILES_STORE').replace(
+            '{}://'.format(urlparse(settings.get('FILES_STORE')).scheme), '')
+        print(s3path)
+
         for f in item['files']:
-            # remove the path and the extension
-            f['name'] = f['path'].split('/')[-1][:-4]
+            f['name'] = f['path'].split('/')[-1].replace('.pdf', '')
+            f['path'] = urljoin(
+                'https://s3-eu-west-1.amazonaws.com',
+                os.path.join(s3path, f['path']))
             table.put_item(Item=f)
-        
         return item
--- docper/settings.py
+++ docper/settings.py
@@ -23,9 +23,11 @@ LOG_LEVEL = 'INFO'
 DOWNLOAD_TIMEOUT = 20
 
 ## Uncomment these lines to enable AWS S3 upload
-# FILES_STORE = 's3://bucker/folder/'
-# AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
-# AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
+# FILES_STORE                 = 's3://bucket/folder/'
+# AWS_ACCESS_KEY_ID           = 'AWS_ACCESS_KEY_ID'
+# AWS_SECRET_ACCESS_KEY       = 'AWS_SECRET_ACCESS_KEY'
+# AWS_REGION_NAME             = 'AWS_REGION_NAME'
+# AWS_DYNAMODB_METADATA_TABLE = 'AWS_DYNAMODB_METADATA_TABLE'
 
 USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7.7'
 DOWNLOAD_DELAY = 0.25
